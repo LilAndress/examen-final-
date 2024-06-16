124a417a61efcea3df8b3fa80aad5c36ef27383e
Andres Rojas <afrojasca@ufpso.edu.co>
2024-06-15 22:25:40 -0500

    HOLA MUNDO

---
+++ examenfinaldios/src/main/java/com/mycompany/examenfinaldios/Examenfinaldios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenfinaldios;
+
+/**
+ *
+ * @author maxis
+ */
+public class Examenfinaldios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
